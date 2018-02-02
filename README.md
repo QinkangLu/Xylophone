4ab34e2434e6bbf34d79a84f14cc8ddc7f06c602
Qinkang Lu <QinkangLu@campus-119-126.ucdavis.edu>
2018-02-02 10:25:12 -0800

    Completed the Xylophone IOS Application

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Xylophone
-
+## Description
+This IOS Application uses the AVFoundation API in Swift and it plays a music note whenever a button is pressed on the phone.
 
 ## Finished App
 <img src="https://github.com/londonappbrewery/Images/blob/master/Xylophone.png" width="400">
---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+    var audioPlayer: AVAudioPlayer?
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        let selectedSoundFileName : String = soundArray[sender.tag-1];
+        playSound(sound: selectedSoundFileName);
+    }
+    
+    func playSound(sound: String){
+        let soundUrl = Bundle.main.url(forResource: sound, withExtension: "wav")!
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl)
+        } catch{
+            print(error)
+        }
+            audioPlayer?.play()
+    }
+    
+
+}
+
